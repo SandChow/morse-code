45a250a67a0aff3b31a7b5fcf94f8c0d3f24894e
SandChow <srisandeep@hotmail.com>
2016-11-30 12:53:36 -0800

    Reorganize

--- morse.sv
+++ morse.sv
@@ -1,47 +1,66 @@
 /* 
-Letter  Morse
-A   .-
-B   -...
-C   -.-.
-D   -..
-E   .
-F   ..-.
-G   --.
-H   ....
-I   ..
-J   .---
-K   -.-
-L   .-..
-M   --
-N   -.
-O   ---
-P   .--.
-Q   --.-
-R   .-.
-S   ...
-T   -
-U   ..-
-V   ...-
-W   .--
-X   -..-
-Y   -.--
-Z   --..
-0   -----
-1   .----
-2   ..---
-3   ...--
-4   ....-
-5   .....
-6   -....
-7   --...
-8   ---..
-9   ----.
+A   .-      M   --      Y   -.--
+B   -...    N   -.      Z   --..
+C   -.-.    O   ---     0   -----
+D   -..     P   .--.    1   .----
+E   .       Q   --.-    2   ..---
+F   ..-.    R   .-.     3   ...--
+G   --.     S   ...     4   ....-
+H   ....    T   -       5   .....
+I   ..      U   ..-     6   -....
+J   .---    V   ...-    7   --...
+K   -.-     W   .--     8   ---..
+L   .-..    X   -..-    9   ----.
 */
-module morse(Clock, Reset, HEX0);
+
+module morse(Clock, Reset, dot, dash, out);
+
+    enum {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,
+          zero,one,two,three,four,five,six,seven,eight,nine,decoding} ps, ns;
+
+    always_comb
+        case (ps)
+            decoding:
+            A:
+            B:
+            C:
+            D:
+            E:
+            F:
+            G:
+            H:
+            J:
+            K:
+            L:
+            M:
+            N:
+            O:
+            P:
+            Q:
+            R:
+            S:
+            T:
+            U:
+            V:
+            W:
+            X:
+            Y:
+            Z:
+            zero:
+            one:
+            two:
+            three:
+            four:
+            five:
+            six:
+            seven:
+            eight:
+            nine:
+
 
     always_ff @(posedge Clock)
         if (Reset)
-            ps <= nothing;
+            ps <= decoding;
         else
             ps <= ns;
 endmodule
